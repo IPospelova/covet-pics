88edf0dd3c222e1950a11212deafed819c459d6c
IPospelova <iri_shka_s@mail.ru>
2020-11-12 15:36:04 +0300

    add - video popup cut off

--- js/common.js
+++ js/common.js
@@ -1,13 +1,22 @@
 $(function() {
 
-	const mySwiper = new Swiper('.swiper-container', {
-		// Optional parameters
-		loop: true,
-	
-		// If we need pagination
-		pagination: {
-			el: '.swiper-pagination',
-		},
+	const galleryThumbs = new Swiper('.gallery-thumbs', {
+		spaceBetween: 10,
+		slidesPerView: 4,
+		freeMode: true,
+		watchSlidesVisibility: true,
+		watchSlidesProgress: true,
 	})
 
-});
+	const galleryTop = new Swiper('.gallery-top', {
+		spaceBetween: 10,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		thumbs: {
+			swiper: galleryThumbs
+		}
+	})
+
+})
